97031c300184c4d5e3e8e433d55b284239af205b
Christine Chang <xtnchang@gmail.com>
2015-05-03 16:37:42 -0700

    Updated pins.rb to :storage, :bucket, and :s3_credentials

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,10 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
+	has_attached_file :image, 
+		:styles => { :medium => "300x300>", :thumb => "100x100>" }
+		:storage => :s3,
+        :bucket => 'hello-pinteresting',
+        :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 	
 	validates :image, presence: true
