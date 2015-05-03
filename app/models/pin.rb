2ef5f42dace5f3bb2f3388a6f405a1316e663af0
Christine Chang <xtnchang@gmail.com>
2015-05-03 16:44:48 -0700

    Added comma in pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, 
-		:styles => { :medium => "300x300>", :thumb => "100x100>" }
+		:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 		:storage => :s3,
         :bucket => 'hello-pinteresting',
         :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
