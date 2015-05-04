63ffd7e83e290993996e46c171b126497eb5ad4e
Christine Chang <xtnchang@gmail.com>
2015-05-03 20:02:48 -0700

    In production.rb, change AWS to S3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, 
 		:styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 		:storage => :s3,
-        :bucket => 'hello-pinteresting',
+        :bucket => 'hellopinteresting',
         :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 	
